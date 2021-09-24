692236debbfadbd3914668ced419c8ea7899f7ce
KrokodilGenadiy <alex.usevich@gmail.com>
2021-09-24 23:22:42 +0300

    Added functionality to the search bar

--- app/src/main/java/com/zaus_app/moviefrumy/HomeFragment.kt
+++ app/src/main/java/com/zaus_app/moviefrumy/HomeFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.zaus_app.moviefrumy.Database.filmsDataBase
 import com.zaus_app.moviefrumy.databinding.FragmentHomeBinding
+import java.util.*
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -26,9 +29,44 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        binding.searchView.setOnClickListener {
+            binding.searchView.isIconified = false
+        }
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            //Этот метод отрабатывает при нажатии кнопки "поиск" на софт клавиатуре
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            //Этот метод отрабатывает на каждое изменения текста
+            override fun onQueryTextChange(newText: String): Boolean {
+                //Если ввод пуст то вставляем в адаптер всю БД
+                if (newText.isEmpty()) {
+                    updateData(filmsDataBase as MutableList<Film>)
+                    return true
+                }
+                //Фильтруем список на поискк подходящих сочетаний
+                val result = filmsDataBase.filter {
+                    //Чтобы все работало правильно, нужно и запрос, и имя фильма приводить к нижнему регистру
+                    it.title.lowercase(Locale.getDefault())
+                        .contains(newText.lowercase(Locale.getDefault()))
+                }
+                //Добавляем в адаптер
+                updateData(result as MutableList<Film>)
+                return true
+            }
+        })
+        initRecycler()
+        //Кладем нашу БД в RV
+        updateData(filmsDataBase as MutableList<Film>)
+    }
+
+    fun initRecycler() {
         //находим наш RV
         _binding?.mainRecycler?.apply {
-            filmsAdapter = FilmAdapter(object : FilmAdapter.OnItemClickListener{
+            filmsAdapter = FilmAdapter(object : FilmAdapter.OnItemClickListener {
                 override fun click(film: Film) {
                     (requireActivity() as MainActivity).launchDetailsFragment(film)
                 }
@@ -41,16 +79,15 @@ class HomeFragment : Fragment() {
             val decorator = ItemDecorator(8)
             addItemDecoration(decorator)
         }
-        //Кладем нашу БД в RV
-        updateData(Database.filmsDataBase as MutableList<Film>)
     }
 
-    fun updateData(newList: MutableList<Film>){
+    fun updateData(newList: MutableList<Film>) {
         val oldList = filmsAdapter.getItems()
-        val productDiff = FilmDiff(oldList,newList)
+        val productDiff = FilmDiff(oldList, newList)
         val diffResult = DiffUtil.calculateDiff(productDiff)
         filmsAdapter.setItems(newList)
         diffResult.dispatchUpdatesTo(filmsAdapter)
     }
+
 
 }
