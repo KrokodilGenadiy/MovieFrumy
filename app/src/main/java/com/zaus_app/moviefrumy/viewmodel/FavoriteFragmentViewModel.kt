efc5c61610392753653b0811fa2350e7a9de0d54
KrokodilGenadiy <alex.usevich@gmail.com>
2021-12-11 23:10:07 +0300

    Added DI using Koin

--- app/src/main/java/com/zaus_app/moviefrumy/viewmodel/FavoriteFragmentViewModel.kt
+++ app/src/main/java/com/zaus_app/moviefrumy/viewmodel/FavoriteFragmentViewModel.kt
@@ -5,11 +5,14 @@ import androidx.lifecycle.ViewModel
 import com.zaus_app.moviefrumy.App
 import com.zaus_app.moviefrumy.domain.Film
 import com.zaus_app.moviefrumy.domain.Interactor
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.koin.java.KoinJavaComponent.inject
 
-class FavoriteFragmentViewModel : ViewModel() {
+class FavoriteFragmentViewModel : ViewModel(), KoinComponent {
     val filmsListLiveData: MutableLiveData<List<Film>> = MutableLiveData()
     //Инициализируем интерактор
-    private var interactor: Interactor = App.instance.interactor
+    private val interactor: Interactor by inject()
 
     init {
         val films = interactor.getFavoriteFilms()
