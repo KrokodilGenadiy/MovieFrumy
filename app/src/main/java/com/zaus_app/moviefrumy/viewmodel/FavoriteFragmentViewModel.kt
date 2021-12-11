a29c011028cb9ad5d72617d8f2bcced912571e70
KrokodilGenadiy <alex.usevich@gmail.com>
2021-12-12 01:32:55 +0300

    Migrating to Hilt

--- app/src/main/java/com/zaus_app/moviefrumy/viewmodel/FavoriteFragmentViewModel.kt
+++ app/src/main/java/com/zaus_app/moviefrumy/viewmodel/FavoriteFragmentViewModel.kt
@@ -7,7 +7,7 @@ import com.zaus_app.moviefrumy.domain.Film
 import com.zaus_app.moviefrumy.domain.Interactor
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
-import org.koin.java.KoinJavaComponent.inject
+
 
 class FavoriteFragmentViewModel : ViewModel(), KoinComponent {
     val filmsListLiveData: MutableLiveData<List<Film>> = MutableLiveData()
