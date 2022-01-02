8320e23e0099b068aa334b5228460b703b2a6059
KrokodilGenadiy <alex.usevich@gmail.com>
2022-01-03 01:50:51 +0300

    Some small changes

--- app/src/main/java/com/zaus_app/moviefrumy/di/modules/DatabaseModule.kt
+++ app/src/main/java/com/zaus_app/moviefrumy/di/modules/DatabaseModule.kt
@@ -18,7 +18,7 @@ class DatabaseModule {
         Room.databaseBuilder(
             context,
             AppDatabase::class.java,
-            "film_db"
+            "films_db"
         ).build().filmDao()
 
     @Provides
