f3a99c7f66b9f73f8e5554da1a5b3250ae591b07
KrokodilGenadiy <alex.usevich@gmail.com>
2022-02-01 17:31:10 +0300

    change logic of getting items for film_list, now it works like this: 1)Getting films from api 2)putting them in db 3)and from db using livedata displaying them on ui

--- app/src/main/java/com/zaus_app/moviefrumy/data/dao/FilmDao.kt
+++ app/src/main/java/com/zaus_app/moviefrumy/data/dao/FilmDao.kt
@@ -9,11 +9,9 @@ import com.zaus_app.moviefrumy.data.entity.Film
 
 @Dao
 interface FilmDao {
-    //Запрос на всю таблицу
     @Query("SELECT * FROM cached_films")
     fun getCachedFilms(): LiveData<List<Film>>
 
-    //Кладём списком в БД, в случае конфликта перезаписываем
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(list: List<Film>)
 
